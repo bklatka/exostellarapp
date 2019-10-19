3311a54b24ec3dd8ea06cb4c63f6a0346c0ba62d
Bernard Klatka <bernard.klatka@netguru.co>
2019-10-19 13:12:12 +0200

    chore: add prettier

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
-import React from 'react';
-import './App.css';
-import { createStore } from 'redux';
-import rootReducer from './store/reducers';
-import { Provider } from 'react-redux'
-import TestComponent from './components/testComponent/testComponent';
+import React from "react";
+import "./App.css";
+import { createStore } from "redux";
+import rootReducer from "./store/reducers";
+import { Provider } from "react-redux";
+import TestComponent from "./components/testComponent/testComponent";
 
-
-const store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
-
+const store = createStore(
+  rootReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 function App() {
   return (
-      <Provider store={store}>
-    <div className="App">
-      <TestComponent />
-    </div>
-      </Provider>
+    <Provider store={store}>
+      <div className="App">
+        <h1>Hello</h1>
+        <TestComponent />
+      </div>
+    </Provider>
   );
 }
 
