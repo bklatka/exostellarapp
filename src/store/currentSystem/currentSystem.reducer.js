add573b65cea19dfdee98774038e05ec4426e5f3
Dariusz Mendocha <mendocha@red-sky.pl>
2019-10-20 01:55:07 +0200

    Remove planet from system

--- src/store/currentSystem/currentSystem.reducer.js
+++ src/store/currentSystem/currentSystem.reducer.js
@@ -2,35 +2,10 @@ import {
   ADD_PLANET_TO_SYSTEM,
   ADD_STAR_TO_SYSTEM,
   ASSIGN_PLANET_TO_ORBIT,
+  REMOVE_PLANET_FROM_SYSTEM,
   RESET_CURRENT_SYSTEM,
   UNASSIGN_PLANET_FROM_ORBIG
 } from "./currentSystem.actions";
-
-const mockPlanets = [
-  {
-    id: 1,
-    name: "Earth",
-    mass: 123,
-    type: "Some earth tyupe",
-    planetThumbnailUrl:
-      "https://media.npr.org/assets/img/2013/03/06/bluemarble3k-smaller-nasa_custom-644f0b7082d6d0f6814a9e82908569c07ea55ccb-s800-c85.jpg"
-  },
-  {
-    id: 2,
-    name: "Mars",
-    mass: 3423,
-    type: "Some earth tyupe",
-    planetThumbnailUrl:
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/0/02/OSIRIS_Mars_true_color.jpg/1200px-OSIRIS_Mars_true_color.jpg"
-  },
-  {
-    id: 3,
-    name: "Venus",
-    mass: 534,
-    type: "Some earth tyupe",
-    planetThumbnailUrl: "https://en.es-static.us/upl/2019/09/Venus-dayside.png"
-  }
-];
 
 export const initialState = {
   planets: [],
@@ -67,10 +42,11 @@ export const currentSystemReducer = (state = initialState, action = {}) => {
       });
       return { ...state, planets };
     }
-    case ADD_STAR_TO_SYSTEM:
+    case ADD_STAR_TO_SYSTEM: {
       return { ...state, star: { ...action.payload } };
+    }
 
-    case ADD_PLANET_TO_SYSTEM:
+    case ADD_PLANET_TO_SYSTEM: {
       const planets = state.planets.filter(
         planet => planet.id !== action.payload.id
       );
@@ -84,6 +60,13 @@ export const currentSystemReducer = (state = initialState, action = {}) => {
           }
         ]
       };
+    }
+    case REMOVE_PLANET_FROM_SYSTEM: {
+      const planets = state.planets.filter(
+        planet => planet.id !== action.payload
+      );
+      return { ...state, planets };
+    }
     case RESET_CURRENT_SYSTEM:
       return initialState;
     default:
